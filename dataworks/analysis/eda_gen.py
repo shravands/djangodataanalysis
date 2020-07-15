8e34ceca68fa8da595ccbf41e2a35087f2c1ea3e
DESKTOP-I516M6U\lenovo <shravands66@gmail.com>
2020-07-15 11:37:28 +0530

    adding the chnage in file name for report genearted

--- dataworks/analysis/eda_gen.py
+++ dataworks/analysis/eda_gen.py
@@ -16,10 +16,11 @@ output_file_name = 'eda_profile'+ str(current_time()) + '.html'
 def eda_csv(file_name, host_val):
     """getting the data from the file"""
     result = {}
+    file_name_print = (file_name.split(sep='.')[-2]).split(sep='/')[-1]
     df = pd.read_csv(file_name, error_bad_lines=False, encoding="cp1252")
-    profile = pandas_profiling.ProfileReport(df, title='Data profile', explorative=True)
+    profile = pandas_profiling.ProfileReport(df, title='Data profile : '+file_name_print, explorative=True)
     folder = 'eda_profile/'
-    profile_file = profile.to_file('media/eda_folder/'+output_file_name)
+    profile_file = profile.to_file('media/eda_folder/'+file_name_print+output_file_name)
 
     describe = df.describe() #getting the overview of the df
     head = df.head()
@@ -27,7 +28,7 @@ def eda_csv(file_name, host_val):
     result['dimensions'] = df.shape
     result['sample_data'] = head.to_dict()
     result['data_description'] = describe.to_dict()
-    result['profile_link'] = {'file_path':'http://'+host_val+'/media/eda_folder/'+output_file_name}
+    result['profile_link'] = {'file_path':'http://'+host_val+'/media/eda_folder/'+file_name_print+output_file_name}
     result['status_code'] = 200 
     result['host'] = socket.gethostbyname(socket.gethostname())
     result['host_val'] = host_val
--- dataworks/analysis/views.py
+++ dataworks/analysis/views.py
@@ -7,5 +7,11 @@ def data_analizer(request):
     req_met = request.method  # holds the request method
     host_val = request.META['HTTP_HOST']
     file_name = request.GET.get('file_name')  # getting the query strings passed in url
-    data = eda_csv(file_name, host_val)
-    return JsonResponse(data, safe=False)
+    if not file_name:
+        return JsonResponse({"msg":"please pass the file name under argument file_name",
+                             "example":"?file_name=file_name.csv"})
+    elif not file_name.lower().endswith('.csv'):
+        return JsonResponse({"msg":"The file is not of csv type"})
+    else:
+        data = eda_csv(file_name, host_val)
+        return JsonResponse(data, safe=False)
