c6ce5e42b6a96f19d47987d44bceb8e1845eb6e7
DESKTOP-I516M6U\lenovo <shravands66@gmail.com>
2020-07-21 20:37:34 +0530

    adding the changes for saving data to s3

--- dataworks/analysis/eda_gen.py
+++ dataworks/analysis/eda_gen.py
@@ -7,20 +7,40 @@ matplotlib.use('Agg') #handling the runtime error for the 2nd instance
 import socket
 import time
 import os
-from django.core.files.storage import FileSystemStorage
+import boto3
+from boto3.s3.transfer import S3Transfer
 
 
 current_time = lambda: int(round(time.time()))
 output_file_name = 'eda_profile'+ str(current_time()) + '.html'
 
-def eda_csv(file_name, host_val):
+credentials = { 
+    'aws_access_key_id': 'AKIA4IY2R63BUN2HGB5M',
+    'aws_secret_access_key': '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'
+}
+
+def eda_csv(file_name, host_val, s3_value):
     """getting the data from the file"""
     result = {}
+    file_url = 'o'
     file_name_print = (file_name.split(sep='.')[-2]).split(sep='/')[-1]
     df = pd.read_csv(file_name, error_bad_lines=False, encoding="cp1252")
+    # creating the pandas profile and saving the data to the file.
+
     profile = pandas_profiling.ProfileReport(df, title='Data profile : '+file_name_print, explorative=True)
-    folder = 'eda_profile/'
     profile_file = profile.to_file('media/eda_folder/'+file_name_print+output_file_name)
+    profile_link = 'http://'+host_val+'/media/eda_folder/'+file_name_print+output_file_name
+
+    if int(s3_value) == 1:
+        #s3 configuration, uploading the data for s3
+        filename = file_name_print+output_file_name
+        key = filename
+        bucket = 'bucketstoredata'
+        client = boto3.client('s3', 'ap-south-1', **credentials)
+        transfer = S3Transfer(client)
+        transfer.upload_file('media/eda_folder/'+file_name_print+output_file_name, bucket, key , extra_args={'ACL': 'public-read', 'ContentType':'text/html'})
+        file_url = '%s/%s/%s' % (client.meta.endpoint_url, bucket, key)    
+        
 
     describe = df.describe() #getting the overview of the df
     head = df.head()
@@ -28,7 +48,8 @@ def eda_csv(file_name, host_val):
     result['dimensions'] = df.shape
     result['sample_data'] = head.to_dict()
     result['data_description'] = describe.to_dict()
-    result['profile_link'] = {'file_path':'http://'+host_val+'/media/eda_folder/'+file_name_print+output_file_name}
+    result['profile_link'] = profile_link
+    result['s3_url'] = file_url
     result['status_code'] = 200 
     result['host'] = socket.gethostbyname(socket.gethostname())
     result['host_val'] = host_val
